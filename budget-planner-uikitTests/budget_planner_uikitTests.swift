8d6d56d9d87bb3d913cec7052c37319139b45b6c
Shayan Sadjady <shayansadjady21@gmail.com>
2025-12-19 15:43:13 +0000

    Initial Commit

---
+++ budget-planner-uikitTests/budget_planner_uikitTests.swift
@@ -0,0 +1,17 @@
+//
+//  budget_planner_uikitTests.swift
+//  budget-planner-uikitTests
+//
+//  Created by Shayan Sadjady on 19/12/2025.
+//
+
+import Testing
+@testable import budget_planner_uikit
+
+struct budget_planner_uikitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
